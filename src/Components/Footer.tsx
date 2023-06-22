77c39d448d84d492df73a0916a8f8c8a860901f1
jaumdev1 <joao280799@gmail.com>
2023-06-22 13:52:21 -0300

    first commit

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const MenuInferior = () => {
+  return (
+    <footer className="menu-inferior">
+      <div className="texto-centralizado">savegram</div>
+    </footer>
+  );
+};
+
+export default MenuInferior;
